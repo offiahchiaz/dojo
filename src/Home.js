753772819308a6daf02bfabfc50b3d8849847d36
offiahchiaz <offiahchiaz@gmail.com>
2021-11-01 11:41:22 +0100

    Added hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+    const blogList = blogs.map((blog) => {
+        return (
+            <div className="blog-preview" key={ blog.id }>
+                <h2> { blog.title } </h2>
+                <p>Written by { blog.author }</p>
+            </div>
+        );
+    });
+    
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            { blogList }
         </div>
      );
 }
