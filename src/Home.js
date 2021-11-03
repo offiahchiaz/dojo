0084c9b26d7064220e858919a083c84dad36250e
offiahchiaz <offiahchiaz@gmail.com>
2021-11-03 01:09:53 +0100

    Updated Home component with custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,16 @@
-import { useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
 
-    const blogList = blogs.map((blog) => {
-        return (
-            <div className="blog-preview" key={ blog.id }>
-                <h2> { blog.title } </h2>
-                <p>Written by { blog.author }</p>
-            </div>
-        );
-    });
-    
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
     return ( 
         <div className="home">
-            { blogList }
+            { error && <div> {  error } </div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
         </div>
      );
 }
