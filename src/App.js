bee665db00a8c314c9e45daa36d0f003d053ef55
offiahchiaz <offiahchiaz@gmail.com>
2021-11-04 10:36:18 +0100

    Updated App to use react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title = 'Welcome to the new blog';
+  //const title = 'Welcome to the new blog';
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/create"><Create /></Route>
+          </Switch>
+        </div>
     </div>
+    </Router>
   );
 }
 
